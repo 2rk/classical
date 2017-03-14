6c5a9f7e16b6355172391b5ea59bb0e5a32b0ce5
Caitlin Palmer-Bright <Caitlin@Caitlins-MacBook-Air-2.local>
2017-03-14 19:04:24 +1000

    Replaced getter and setter methods with attr_reader/attr_accessor

--- models/human.rb
+++ models/human.rb
@@ -1,48 +1,17 @@
 class Human
 
+  attr_accessor :job_title, :age, :drinking_age
+  attr_reader :name
+
   def initialize value
     @name = value
   end
 
-  def name
-    @name
-  end
-
   def legal_name
-    first_name = @name.split.first
-    last_name = @name.split.last
-
-    last_name + ", " + first_name
-
-    # @name.split(' ', 2).reverse.join(', ')
-  end
-
-  # attr_accessor :job_title
-  def job_title=(value)
-    @job_title = value
-  end
-
-  def job_title
-    @job_title
-  end
-
-  def age=(value)
-    @age = value
-  end
-
-  def age
-    @age
-  end
-
-  def drinking_age=(value)
-    @drinking_age = value
-  end
-
-  def drinking_age
-    @drinking_age
+    @name.split().reverse.join(', ')
   end
 
   def can_drink?
     return @age >= @drinking_age
-  end 
+  end
 end
